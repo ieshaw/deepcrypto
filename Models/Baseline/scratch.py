a8417ea2db372776d93c9dc95413e01e717b0426
Ian Shaw <ianedwardshaw@gmail.com>
2018-02-20 16:24:34 -0800

    Data Plots and writing directory

--- Models/Baseline/scratch.py
+++ Models/Baseline/scratch.py
@@ -1,14 +1,10 @@
 __author__ = 'Ian'
 
+import matplotlib.pyplot as plt
 import os
 import pandas as pd
 from Data.scripts.data import data
 
-#set can be 'train', 'cross_val', or 'test'
-
 X,Y = data.import_data(set= 'train')
 
-
-print(X.head())
-
-print(Y.head())
+print((pd.to_datetime(X.index, format='%Y-%m-%d %H:%M:%S')).date)
---
+++ Models/Baseline/time_series_plots.py
@@ -0,0 +1,39 @@
+__author__ = 'Ian'
+
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+from Data.scripts.data import data
+
+def time_series_plot(returns_series, ticker = 'BTC'):
+
+    fig_hist = plt.figure()
+    plt.title('Historgram of {0} {1} Returns'.format(ticker, 'Hourly'))
+    returns_series.hist(bins= 100, log= True)
+
+    #generate time series of returns
+
+    fig_ts = plt.figure()
+    plt.title('Time Series of {0} {1} Returns'.format(ticker, 'Hourly'))
+    ((returns_series + 1).cumprod()).plot(xticks=(pd.to_datetime(returns_series.index, format='%Y-%m-%d %H:%M:%S')).date,
+                                          rot=45)
+
+    #save results
+
+    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/Baseline/plots'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    fig_hist.savefig('{0}/{1}_hist.png'.format(output_dir, ticker))
+    fig_ts.savefig('{0}/{1}_ts.png'.format(output_dir, ticker))
+
+#set can be 'train', 'cross_val', or 'test'
+
+X,Y = data.import_data(set= 'train')
+
+
+coins = ['ETH', 'XRP','LTC', 'DASH', 'XMR']
+
+
+for coin in coins:
+    time_series_plot(returns_series= X[coin + 'return'], ticker= coin)
