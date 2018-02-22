8d9fd8121f0608f38de5e30d22bd8fc05a5ddddd
Ian Shaw <ianedwardshaw@gmail.com>
2018-02-22 09:09:25 -0800

    Updating evaluate Strategy script

--- Models/Baseline/evaluate_strategy.py
+++ Models/Baseline/evaluate_strategy.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
 from Data.scripts.data import data
+from pandas.tools.plotting import autocorrelation_plot
 
 def run_strategy(Y_pred, Returns_df):
 
@@ -22,6 +25,22 @@ def run_strategy(Y_pred, Returns_df):
     return strat_returns_series
 
     #evaluate the
+
+def strat_metrics(Strat_series):
+
+    metrics = {}
+
+    metrics['return'] = strat_series[-1]
+
+    risk_free = 0
+
+    metrics['sharpe'] = ( (strat_series[-1]-1) - risk_free)/(np.std(strat_series))
+
+    metrics['max_drawdown'] = (1 - strat_series.div(strat_series.cummax())).max()
+
+    return metrics
+
+
 X,Y = data.import_data(set= 'train')
 
 coins = ['ETH', 'XRP','LTC', 'DASH', 'XMR']
@@ -29,31 +48,31 @@ coins = ['ETH', 'XRP','LTC', 'DASH', 'XMR']
 returns_df = X[[coin + 'return' for coin in coins]]
 returns_df.columns = coins
 
-Y_ones = (Y*0) + 1
-
-
-
-output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/Baseline/plots'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-fig_ts = plt.figure()
-
-strat_series = (run_strategy(Y_ones, returns_df)).cumprod()
-
-strat_series.index = pd.to_datetime(strat_series.index, format='%Y-%m-%d %H:%M:%S')
-
-strat_series.plot(rot= 45)
-# plt.gcf().autofmt_xdate()
-# myFmt = mdates.DateFormatter('%m/%Y')
-# plt.gca().xaxis.set_major_formatter(myFmt)
-
-# locator = mdates.MonthLocator()
-# plt.gca().xaxis.set_major_locator(locator)
+# autocorrelation_plot(returns_df[coins[3])
 #
-# plt.gcf().autofmt_xdate()
-plt.xlabel('Date')
-plt.ylabel('Returns')
-plt.title('Time Series of Equal Investment Returns')
+# plt.show()
+# Y_ones = (Y*0) + 1
+#
+# # print(Y.sum().sum()/Y_ones.sum().sum())
+# #
+# # print('Accuracy: {}'.format(accuracy_score(Y.values, Y_ones.values)))
+#
+# output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/Baseline/plots'
+# if not os.path.exists(output_dir):
+#     os.makedirs(output_dir)
+#
+# fig_ts = plt.figure()
+#
+# strat_series = (run_strategy(Y_ones, returns_df)).cumprod()
+#
+# strat_series.index = pd.to_datetime(strat_series.index, format='%Y-%m-%d %H:%M:%S')
+#
+# print(strat_metrics(strat_series))
 
-fig_ts.savefig('{0}/baseline_ts.png'.format(output_dir))
+##Plotting
+# strat_series.plot(rot= 45)
+# plt.xlabel('Date')
+# plt.ylabel('Returns')
+# plt.title('Time Series of Equal Investment Returns')
+#
+# fig_ts.savefig('{0}/baseline_ts.png'.format(output_dir))
